581eb22262ec12311fb9f6724a737b2269bb9d03
spikemillion <spikemillion@gmail.com>
2017-01-30 21:25:43 +0100

    002

---
+++ Assets/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG
+
+{
+public class CameraFollow : MonoBehaviour {
+
+	GameObject player;
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void lateUpdate () 
+	{
+		transform.position = player.transform.position;
+	}
+}
+}
